32e427816f7aded86c9d30eaad988d594dc85d4d
Zach Sullivan <zachsully@gmail.com>
2017-02-20 16:42:14 -0800

    added simple command line inteface, though none of the functionality is there. Need to come up with a good interface for selecting subsections of files

--- haskell/Plotting/Density.hs
+++ haskell/Plotting/Density.hs
@@ -1,6 +1,7 @@
 module Plotting.Density where
 
-import Graphics.Rendering.Chart       
+import Graphics.Gnuplot.Simple
+import Data.Number.LogFloat
 
 -- | Simple density is a histogram where the weights of all the samples are 1
 simpleDensity :: Num a => [a] -> p
@@ -8,4 +9,4 @@ simpleDensity = undefined
 
 -- | takes weight,value pairs and produces a histogram
 density :: Num a => [(LogFloat, a)] -> p
-density = undefined        
+density = undefined
--- haskell/Plotting/Plotter.hs
+++ haskell/Plotting/Plotter.hs
@@ -1,4 +1,33 @@
-module Plotting.Plotter where
+module Main where
+
+import Options.Applicative
+import Data.Monoid
+
+data Options = Options { fileIn   :: String
+                       , mfileOut :: Maybe String }
+
+options :: Parser Options
+options = Options
+  <$> strArgument (  metavar "INPUT"
+                  <> help "input dataset")
+  <*> (optional $ strOption (  short 'o'
+                            <> metavar "OUTPUT"
+                            <> help "output file"))
+
+parseOpts :: IO Options
+parseOpts = execParser $ info (helper <*> options)
+                       $ fullDesc <> progDesc "A simple plotting program"
+
+
+data DataType = DSV Char
+  deriving Show
+
+data PlotType
+  = Density
+  | WeightedDensity
+  deriving Show
 
 main :: IO ()
-main = return ()
+main = do
+  opts <- parseOpts
+  return ()
